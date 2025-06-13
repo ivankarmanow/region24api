31652544055317b29bad10564754751016319ec1
Ivan Karmanov <ivan@karmanow.ru>
2025-06-13 13:27:06 +0700

    local changes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.dependencies import config
@@ -6,6 +7,14 @@ from app.api import admin, admin_auth, client, contact, history, order, project,
 from app.error import exception_handler, http_exception_handler
 
 app = FastAPI(title="Region24 API", summary="API веб-сайта Регион24", version="0.1.0", swagger_ui_parameters = {"docExpansion":"none"})
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080", "http://localhost:8080/admin"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(Exception, exception_handler)
